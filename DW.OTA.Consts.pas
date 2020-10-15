db71ac1569a9a23b4d11d3ed80c453f22fbba5a2
Dave Nottage <davidn@radsoft.com.au>
2020-10-15 16:03:23 +1030

    Added consts for use in Codex

--- DW.OTA.Consts.pas
+++ DW.OTA.Consts.pas
@@ -16,12 +16,25 @@ uses
 
 const
   cProjectPlatforms: array[TProjectPlatform] of string = (
-    'Android', 'Android64', ciOSDevice32Platform , ciOSDevice64Platform, ciOSSimulator32Platform, cLinux64Platform, cOSX32Platform, cOSX64Platform,
+    'Android', 'Android64', ciOSDevice32Platform, ciOSDevice64Platform, ciOSSimulator32Platform, cLinux64Platform, cOSX32Platform, cOSX64Platform,
     cWin32Platform, cWin64Platform
   );
 
   cProjectPlatformsShort: array[TProjectPlatform] of string = (
-    'Android', 'Android64', 'iOS32' , 'iOS64', 'iOSSim32', 'Linux64', 'macOS32', 'macOS64', 'Win32', 'Win64'
+    'Android', 'Android64', 'iOS32', 'iOS64', 'iOSSim32', 'Linux64', 'macOS32', 'macOS64', 'Win32', 'Win64'
+  );
+
+  cProjectPlatformsLong: array[TProjectPlatform] of string = (
+    'Android 32-bit', 'Android 64-bit', 'iOS 32-bit', 'iOS 64-bit', 'iOS Simulator', 'Linux 64-bit', 'macOS 32-bit', 'macOS 64-bit',
+    'Windows 32-bit', 'Windows 64-bit'
+  );
+
+  cProjectPlatformDefaultBuildType: array[TProjectPlatform] of string = (
+    'Development', 'Development', 'Development', 'Development', 'Development', '', 'Nornal', 'Normal', 'Normal', 'Normal'
+  );
+
+  cProjectPlatformsRegistry: array[TProjectPlatform] of string = (
+    'Android32', 'Android64', 'iOSDevice32' , 'iOSDevice64', 'iOSSimulator', 'Linux64', 'OSX32', 'OSX64', 'Win32', 'Win64'
   );
 
   cProjectConfigurations: array[TProjectConfiguration] of string = ('Debug', 'Release');
@@ -53,14 +66,13 @@ const
   );
   cAppleProjectPlatforms: set of TProjectPlatform = [TProjectPlatform.iOSDevice32, TProjectPlatform.iOSDevice64,
     TProjectPlatform.macOS32, TProjectPlatform.macOS64];
+  cIOSProjectPlatforms: set of TProjectPlatform = [TProjectPlatform.iOSDevice64];
   cMacOSProjectPlatforms: set of TProjectPlatform = [TProjectPlatform.macOS32, TProjectPlatform.macOS64];
   cAndroidProjectPlatforms: set of TProjectPlatform = [TProjectPlatform.Android32, TProjectPlatform.Android64];
 
   cTabChar = #9;
   cLineFeedChar = #10;
   cCarriageReturnChar = #13;
-
-  cValidControlChars: set of Char = [cTabChar, cLineFeedChar, cCarriageReturnChar];
 
 implementation
 
